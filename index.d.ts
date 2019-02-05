83b4503a90b7033afd677cd29e83891bcaec37fe
Nick Taylor <nick@iamdeveloper.com>
2019-02-04 21:33:19 -0500

    Removed module declaration in types.

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,20 @@
-declare module 'webext-options-sync' {
-	export interface DefineOptions {
-		defaults: Record<string, string|boolean>;
-		migrations: [(options: {disabledFeatures: string}) => void, () => void];
+export interface DefineOptions {
+	defaults: Record<string, string|boolean>;
+	migrations: [(options: {disabledFeatures: string}) => void, () => void];
+}
+
+export default class OptionsSync {
+	static migrations: {
+		removeUnused: () => void;
 	}
 
-	export default class OptionsSync {
-		static migrations: {
-			removeUnused: () => void;
-		}
+	getAll: <T>() => Promise<T>;
 
-		getAll: <T>() => Promise<T>;
+	setAll: (newOptions: Record<string, string|boolean>) => Promise<void>;
 
-		setAll: (newOptions: Record<string, string|boolean>) => Promise<void>;
+	set: (newOptions: Record<string, string|boolean>) => Promise<void>;
 
-		set: (newOptions: Record<string, string|boolean>) => Promise<void>;
+	syncForm: (selector: string|HTMLFormElement) => void;
 
-		syncForm: (selector: string|HTMLFormElement) => void;
-
-		define: (options: DefineOptions) => void;
-	}
+	define: (options: DefineOptions) => void;
 }
