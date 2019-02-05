9586d3014f08fb352cf26d0dd5317879c780c117
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-05 10:43:25 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,26 +2,29 @@
  * Handler signature for when an extension updates.
  */
 export interface ExtensionUpdateHandler {
-	(savedOptions: Record<string,string|boolean>, defaults: Record<string, string|boolean>): void;
+	(savedOptions: Record<string, string | boolean>, defaults: Record<string, string | boolean>): void;
 }
 
 /**
  * @example
+ *
  * {
- * 	defaults: { // recommended
+ * 	// Recommended
+ * 	defaults: {
  * 		color: 'blue'
  *	},
- * 	migrations: [ // optional
+ * 	// Optional
+ * 	migrations: [
  * 		savedOptions => {
- * 			if(savedOptions.oldStuff) {
- * 				delete savedOptions.oldStuff
+ * 			if (savedOptions.oldStuff) {
+ * 				delete savedOptions.oldStuff;
  * 			}
  * 		}
  * 	],
  * }
  */
 export interface DefineOptions {
-	defaults: Record<string, string|boolean>;
+	defaults: Record<string, string | boolean>;
 	/**
 	 * A list of functions to call when the extension is updated.
 	 */
@@ -32,9 +35,9 @@ export default class OptionsSync {
 	/**
 	 * @constructor Returns an instance linked to the chosen storage.
 	 *
-	 * @param [config={storageName='options'}] Configuration to determine where options are stored.
+	 * @param [config={storageName='options'}] - Configuration to determine where options are stored.
 	 */
-	constructor(config?: {storageName: string });
+	constructor(config?: {storageName: string});
 
 	static migrations: {
 		/**
@@ -49,51 +52,48 @@ export default class OptionsSync {
 	 * @returns Promise that will resolve with **all** the options stored, as an object.
 	 *
 	 * @example
+	 *
 	 * new OptionsSync().getAll().then(options => {
 	 * 	console.log('The user’s options are', options);
-	 * 	if(options.color) {
+	 * 	if (options.color) {
 	 * 		document.body.style.color = color;
 	 * 	}
-	 * });
 	 * });
 	 */
 	getAll: <T>() => Promise<T>;
 
 	/**
-	 * Overrides **all** the options stored with your `options`
+	 * Overrides **all** the options stored with your `options`.
 	 *
-	 * @param {Record<string, string}|boolean>} A map of default options as strings or booleans. The keys will have to match the form fields' `name` attributes.
+	 * @param {Record<string, string | boolean>} newOptions - A map of default options as strings or booleans. The keys will have to match the form fields' `name` attributes.
 	 */
-	setAll: (newOptions: Record<string, string|boolean>) => Promise<void>;
+	setAll: (newOptions: Record<string, string | boolean>) => Promise<void>;
 
 	/**
 	 * Merges new options with the existing stored options.
 	 *
-	 * @param {Record<string, string}|boolean>} A map of default options as strings or booleans. The keys will have to match the form fields' `name` attributes.
+	 * @param {Record<string, string | boolean>} newOptions - A map of default options as strings or booleans. The keys will have to match the form fields' `name` attributes.
 	 */
-	set: (newOptions: Record<string, string|boolean>) => Promise<void>;
+	set: (newOptions: Record<string, string | boolean>) => Promise<void>;
 
 	/**
-	 * Any defaults or saved options will be loaded into the `<form>` and any change will automatically be saved via `chrome.storage.sync`
+	 * Any defaults or saved options will be loaded into the `<form>` and any change will automatically be saved via `chrome.storage.sync`.
 	 *
-	 * @param {string|HTMLFormElement} selector The `<form>` that needs to be synchronized or a CSS selector (one element).
+	 * @param {string | HTMLFormElement} selector - The `<form>` that needs to be synchronized or a CSS selector (one element).
 	 * The form fields' `name` attributes will have to match the option names.
 	 */
-	syncForm: (selector: string|HTMLFormElement) => void;
+	syncForm: (selector: string | HTMLFormElement) => void;
 
 	/**
-	 * To be used in the background only, this is used to initiate the options. It's not required but it's recommended as a way to define which options the extension supports.
+	 * To be used in the background only. This is used to initiate the options. It's not required but it's recommended as a way to define which options the extension supports.
 	 *
-	 * @param  {DefineOptions} options
 	 * @example
 	 *
-	 * Usage:
-	 * 	new OptionsSync().define({
+	 * new OptionsSync().define({
 	 * 	defaults: {
-	 * 	yourStringOption: 'green',
-	 *		 anyBooleans: true,
-	 *		 numbersAreFine: 9001
-	 *		 // Open an issue to discuss more complex fields like multiselects
+	 * 		yourStringOption: 'green',
+	 *		anyBooleans: true,
+	 *		numbersAreFine: 9001
 	 *	 }
 	 * });
 	*/
