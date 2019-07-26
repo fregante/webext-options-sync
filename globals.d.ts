fe776113d7f1d4a9f15eb3a338414cf3a358fc90
Federico Brigante <opensource@bfred.it>
2019-07-26 22:43:09 +0700

    Lint

--- globals.d.ts
+++ globals.d.ts
@@ -1,12 +1,12 @@
 declare module 'dom-form-serializer' {
-	export function serialize (
+	export function serialize(
 		element: HTMLFormElement,
 		options: {
 			exclude?: string[];
 		}
 	): JSONValue;
 
-	export function deserialize (
+	export function deserialize(
 		element: HTMLFormElement,
 		serializedData: JSONValue
 	): void;
