7eedb2e80f95c54bfce2e9386e6e4d56d780c125
Federico Brigante <me@fregante.com>
2022-01-12 17:52:32 +0800

    Lint

--- test/_fixtures.js
+++ test/_fixtures.js
@@ -2,6 +2,6 @@ import chrome from 'sinon-chrome';
 
 global.location = {
 	origin: 'chrome://abc',
-	pathname: '/_generated_background_page.html'
+	pathname: '/_generated_background_page.html',
 };
 global.chrome = chrome;
